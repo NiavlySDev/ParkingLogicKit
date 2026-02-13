9813f8c92dbd799365530dc978dca0d198564704
Virgile Alari <virgile.alari@gmail.com>
2026-02-13 15:36:38 +0100

    Update

--- physique/src/main/java/lml/snir/parkinglogickit/physique/data/AccessDataServiceJPAImpl.java
+++ physique/src/main/java/lml/snir/parkinglogickit/physique/data/AccessDataServiceJPAImpl.java
@@ -1,6 +1,5 @@
 package lml.snir.parkinglogickit.physique.data;
 
-
 import lml.snir.parkinglogickit.metier.entity.Access;
 import lml.snir.persistence.jpa.AbstracCrudServiceJPA;
 
@@ -9,11 +8,11 @@ import lml.snir.persistence.jpa.AbstracCrudServiceJPA;
  * @author Virgile Alari
  */
 public class AccessDataServiceJPAImpl extends AbstracCrudServiceJPA<Access> implements AccessDataService {
-    
-        public AccessDataServiceJPAImpl(String PU) {
+
+    public AccessDataServiceJPAImpl(String PU) {
         super(PU);
     }
-        
+
     @Override
     public Access getById(long id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
