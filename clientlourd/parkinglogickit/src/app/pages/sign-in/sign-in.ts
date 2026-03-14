d3492f3f6a5e77938ae0a57cdd040d0d68249e06
Ethan Chandebois <github.husband810@passinbox.com>
2026-03-14 12:05:42 +0100

    c'est quoi ce souke

--- clientlourd/parkinglogickit/src/app/pages/sign-in/sign-in.ts
+++ clientlourd/parkinglogickit/src/app/pages/sign-in/sign-in.ts
@@ -4,15 +4,17 @@ import { CommonModule } from '@angular/common';
 import { DriverService } from '../../../Rest/DriverService';
 import { Driver } from '../../../Auth/Driver';
 import { Router } from '@angular/router';
-
+import { PrimengModule } from '../../shared/primeng.module';
+import { MessageService } from 'primeng/api';
 // author Ethan
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, PrimengModule],
   templateUrl: './sign-in.html',
   styleUrls: ['./sign-in.css'],
+  providers: [MessageService],
 })
 export class SignIn {
   username: string = '';
@@ -22,7 +24,11 @@ export class SignIn {
   message: string = '';
   messageType: 'success' | 'error' = 'success';
 
-  constructor(private driverService: DriverService, private router: Router) {}
+  constructor(
+    private driverService: DriverService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
 
   // Bouton Home
   goHome(): void {
@@ -47,10 +53,8 @@ export class SignIn {
         this.isLoading = false;
         if (driver.password === this.password) {
           this.setMessage('Connexion réussie 🎉', 'success');
+          this.router.navigate(['/reception']);
           // Redirection automatique après succès
-          setTimeout(() => {
-            this.router.navigate(['/']);
-          }, 500); // délai pour voir le message
         } else {
           this.setMessage('Mot de passe incorrect', 'error');
         }
