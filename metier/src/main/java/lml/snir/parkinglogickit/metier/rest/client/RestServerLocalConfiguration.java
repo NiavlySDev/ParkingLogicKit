ab2bbed4cb59b326ecae2c0fda1bae0610ea6d9a
Virgile Alari <virgile.alari@gmail.com>
2026-04-08 11:23:28 +0200

    rest getall sec

--- metier/src/main/java/lml/snir/parkinglogickit/metier/rest/client/RestServerLocalConfiguration.java
+++ metier/src/main/java/lml/snir/parkinglogickit/metier/rest/client/RestServerLocalConfiguration.java
@@ -12,8 +12,8 @@ public class RestServerLocalConfiguration extends RestServerConfig {
     // http://localhost:8080/ParkingLogicKitServeur/rest/BadgeService
     // http://localhost:8080/ParkingLogicKitServeur/rest/application.wadl
     // http://localhost:8080/ParkingLogicKitServeur/rest/DriverService/getById/HS?login=PLK&pass=PASSPLK
-//    http://localhost:8080/ParkingLogicKitServeur/rest/BadgeService/getByContent/0014511054?login=PLK&pass=PASSPLK
-//  http://localhost:8080/ParkingLogicKitServeur/rest/ParkingService/getByIsFull/0?login=PLK&pass=PASSPLK
+    // http://localhost:8080/ParkingLogicKitServeur/rest/BadgeService/getByContent/0014511054?login=PLK&pass=PASSPLK
+    // http://localhost:8080/ParkingLogicKitServeur/rest/ParkingService/getByIsFull/0?login=PLK&pass=PASSPLK
     private static String url = "http://localhost:8080"; // default value
 
     static {
